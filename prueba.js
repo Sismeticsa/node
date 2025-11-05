e7881819c33e1dcf0d7eae640dc84c2dcca94625
Jamess-lara <eljameslara@gmail.com>
2025-11-05 15:36:11 -0500

    primer commit

---
+++ prueba.js
@@ -0,0 +1,5 @@
+// Este es un script de Node.js que saluda y muestra la versión
+const nombre = "Mundo Node";
+
+console.log(`¡Hola, ${nombre}!`);
+console.log(`Estoy ejecutando Node.js en la versión: ${process.version}`);
